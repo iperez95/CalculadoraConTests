dd7527712f82c28361276881a860f50659fdbeb7
Irene Perez <ireneperezcano95@gmail.com>
2022-02-26 19:05:04 +0100

    ClaseSuma

---
+++ README.md
@@ -0,0 +1,8 @@
+# Calculadora_G11_Edix
+
+## Integrantes del grupo
+- [Irene Perez](https://github.com/iperez95)
+- [Diego Fernandez](https://github.com/DiegoFernandezPerez)
+- [Diego Llive](https://github.com/Diego-Llive)
+- [Adrian Alvarez](https://github.com/adriialvz)
+## Descripción de la tarea
--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -8,5 +8,11 @@ public class Main {
         p1.prod2(-2, -5);
         p1.prod3Nega(5, 9, 0);
         p1.pot1(2, 4445);
+
+        Suma s1= new Suma();
+        System.out.println(s1.suma_enteros(5,4));
+        System.out.println(s1.suma_reales(3.4,4.2));
+        System.out.println(s1.suma_3reales(4.5,2.4,5.7));
+        System.out.println(s1.valor_acumulado(3));
     }
 }
