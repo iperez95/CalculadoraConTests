d634a37d4ddef33312e56623738c5428c082e04d
Irene Perez <ireneperezcano95@gmail.com>
2022-02-26 18:38:05 +0100

    Clase producto

---
+++ src/calculadora/Main.java
@@ -0,0 +1,12 @@
+package calculadora;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Producto p1= new Producto();
+        p1.prod1(5, 1);
+        p1.prod2(-2, -5);
+        p1.prod3Nega(5, 9, 0);
+        p1.pot1(2, 4445);
+    }
+}
