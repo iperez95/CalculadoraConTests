6e7820be39dea4f0ca30ec548d502734388ac46b
Irene Perez <ireneperezcano95@gmail.com>
2022-02-27 12:25:48 +0100

    cambio nombres tests

--- tests/calculadora/ProductoTest.java
+++ tests/calculadora/ProductoTest.java
@@ -15,84 +15,84 @@ class ProductoTest {
     }
 
     @Test
-    @DisplayName("Should throw exception when one of values is negative")
+    @DisplayName("Prod1 Should throw exception when one of values is negative")
     void testProd1ShouldThrowExceptionWhenOneOfValuesIsNegative() {
         Throwable exception = assertThrows(RuntimeException.class, () -> producto.prod1(-1, 2));
         assertEquals("EL NUMERO ES NEGATIVO, NO FUNCIONA LA CALCULADORA", exception.getMessage());
     }
 
     @Test
-    @DisplayName("Should throw exception when one of values is zero")
+    @DisplayName("Prod1 Should throw exception when one of values is zero")
     void testProd1ShouldThrowExceptionWhenOneOfValuesIsZero() {
         Throwable exception = assertThrows(RuntimeException.class, () -> producto.prod1(0, 2));
         assertEquals("POR CERO NO SE PUEDE MULTIPLICAR", exception.getMessage());
     }
 
     @Test
-    @DisplayName("Should return result when positive numbers are passed")
+    @DisplayName("Prod1 Should return result when positive numbers are passed")
     void testProd1ShouldReturnResultWhenPositiveNumbersArePassed() {
         double result = producto.prod1(2, 2);
         assertEquals(4, result);
     }
 
     @Test
-    @DisplayName("Should throw exception when one of values is negative")
+    @DisplayName("Prod2 Should throw exception when one of values is negative")
     void testProd2ShouldThrowExceptionWhenOneOfValuesIsNegative() {
         Throwable exception = assertThrows(RuntimeException.class, () -> producto.prod2(-1, 2));
         assertEquals("EL NUMERO ES NEGATIVO, NO FUNCIONA LA CALCULADORA", exception.getMessage());
     }
 
     @Test
-    @DisplayName("Should throw exception when one of values is zero")
+    @DisplayName("Prod2 Should throw exception when one of values is zero")
     void testProd2ShouldThrowExceptionWhenOneOfValuesIsZero() {
         Throwable exception = assertThrows(RuntimeException.class, () -> producto.prod2(0, 2));
         assertEquals("POR CERO NO SE PUEDE MULTIPLICAR", exception.getMessage());
     }
 
     @Test
-    @DisplayName("Should return result when positive numbers are passed")
+    @DisplayName("Prod2 Should return result when positive numbers are passed")
     void testProd2ShouldReturnResultWhenPositiveNumbersArePassed() {
         double result = producto.prod2(2, 2);
         assertEquals(4, result);
     }
 
     @Test
-    @DisplayName("Should throw exception when one of values is negative")
+    @DisplayName("Prod3Nega Should throw exception when one of values is negative")
     void testProd3NegaShouldThrowExceptionWhenOneOfValuesIsNegative() {
         Throwable exception = assertThrows(RuntimeException.class, () -> producto.prod3Nega(-1, 2, 7));
         assertEquals("EL NUMERO ES NEGATIVO, NO FUNCIONA LA CALCULADORA", exception.getMessage());
     }
 
     @Test
-    @DisplayName("Should throw exception when one of values is zero")
+    @DisplayName("Prod3Nega Should throw exception when one of values is zero")
     void testProd3NegaShouldThrowExceptionWhenOneOfValuesIsZero() {
         Throwable exception = assertThrows(RuntimeException.class, () -> producto.prod3Nega(0, 2, 7));
         assertEquals("POR CERO NO SE PUEDE MULTIPLICAR", exception.getMessage());
     }
 
     @Test
-    @DisplayName("Should return result when positive numbers are passed")
+    @DisplayName(" Prod3Nega Should return result when positive numbers are passed")
     void testProd3NegaShouldReturnResultWhenPositiveNumbersArePassed() {
         double result = producto.prod3Nega(2, 2, 3);
         assertEquals(12, result);
     }
 
     @Test
-    @DisplayName("Should throw exception when one of values is negative")
+    @DisplayName("Pot1 Should throw exception when one of values is negative")
     void testPot1ShouldThrowExceptionWhenOneOfValuesIsNegative() {
         Throwable exception = assertThrows(RuntimeException.class, () -> producto.pot1(-1, 2));
         assertEquals("EL NUMERO ES NEGATIVO, NO FUNCIONA LA CALCULADORA", exception.getMessage());
     }
 
     @Test
-    @DisplayName("Should throw exception when one of values is zero")
+    @DisplayName("Pot1 Should throw exception when one of values is zero")
     void testPot1ShouldThrowExceptionWhenOneOfValuesIsZero() {
         Throwable exception = assertThrows(RuntimeException.class, () -> producto.pot1(0, 2));
         assertEquals("POR CERO NO SE PUEDE MULTIPLICAR", exception.getMessage());
     }
 
     @Test
-    @DisplayName("Should return result when positive numbers are passed")
+    @DisplayName("Pot1 Should return result when positive numbers are passed")
     void testPot1ShouldReturnResultWhenPositiveNumbersArePassed() {
         double result = producto.pot1(2, 3);
         assertEquals(8, result);
